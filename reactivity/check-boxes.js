acfc6d4ef35ba2376aaa34746e0a5f1e7087e2aa
Dario Cangialosi <dario.cangialosi@gmail.com>
2024-12-21 22:42:52 +0100

    first commit

---
+++ reactivity/check-boxes.js
@@ -0,0 +1,22 @@
+
+bind_checkboxes(result1)
+bind_checkboxes(result2)
+
+function bind_checkboxes(cb_group) {
+
+	var cb_list = []
+	cb_group.querySelectorAll('input[type=checkbox]').forEach(
+		cb => {
+			cb_list.push(cb);
+			cb.oninput = cb_handler;
+		}
+	)
+
+	function cb_handler() {
+		var ok = cb_list.every(cb => cb.checked)
+		cb_group.style.backgroundColor = ok ? 'lightgreen' : ""
+	}
+
+	cb_handler()
+
+}
