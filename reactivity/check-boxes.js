e87aa890e0cd60cc410e40c86aed08c2f833fee0
Dario Cangialosi <dario.cangialosi@gmail.com>
2025-01-23 09:07:19 +0100

    refactor: format HTML and JavaScript for consistency

--- reactivity/check-boxes.js
+++ reactivity/check-boxes.js
@@ -4,19 +4,19 @@ bind_checkboxes(result2)
 
 function bind_checkboxes(cb_group) {
 
-	var cb_list = []
-	cb_group.querySelectorAll('input[type=checkbox]').forEach(
-		cb => {
-			cb_list.push(cb);
-			cb.oninput = cb_handler;
-		}
-	)
+    var cb_list = []
+    cb_group.querySelectorAll('input[type=checkbox]').forEach(
+        cb => {
+            cb_list.push(cb);
+            cb.oninput = cb_handler;
+        }
+    )
 
-	function cb_handler() {
-		var ok = cb_list.every(cb => cb.checked)
-		cb_group.style.backgroundColor = ok ? 'lightgreen' : ""
-	}
+    function cb_handler() {
+        var ok = cb_list.every(cb => cb.checked)
+        cb_group.style.backgroundColor = ok ? 'lightgreen' : ""
+    }
 
-	cb_handler()
+    cb_handler()
 
 }
